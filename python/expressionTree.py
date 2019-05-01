72a02c405edba8c92de2ad7fd3a9cba4044f3dde
ArthurBatista04 <arthurbatista04@outlook.com>
2019-05-01 12:17:43 -0300

    evalstep

--- python/expressionTree.py
+++ python/expressionTree.py
@@ -1,59 +1,79 @@
 class Tree(object):
-    def __init__(self):
-        nodes = None
 
     def lexer(self,expression):
-        expressionToken = []
-        expression = expression.replace(" ","") # Remoção dos espaços
-        expressionToken.extend(expression)
+        expression = expression.replace("(","( ")
+        expression = expression.replace(")"," )")
+        expressionToken = expression.split()        
         return expressionToken
     
     
-    def greaterPrecedence(self,operator1,operator2): #verifica se o operator1 tem maior precedência se comparado ao 2
-        if operator1 =="+" and operator2 == "*": return False
-        elif operator1 =="+" and operator2 == "/": return False 
-        elif operator1 =="-" and operator2 == "*": return False
-        elif operator1 =="-" and operator2 == "/": return False    
-        return True #Neste caso, comparamos (+ com -) ou (* com /)
+    def greaterPrecedence(self,operator1,operator2,operators): #verifica se o operator1 tem maior precedência se comparado ao 2
+        if operator2 not in operators: #Verifica se o topo da pilha é uma operacao e nao "(" ou ")"
+            return False
+        elif operator2 == "/" or operator2 == "*": #Se o topo da fila contem operatores de de multiplicacao ou divisao, certamente terá maior precedência
+            return True
+        elif operator1 == "/" or operator1 == "*":   
+            return False    
+        return True # Ambos os operadores têm mesma precedência
     
+
+
     def paser(self,token):
         queue = []
         stack = []
-        while not token:
+        operators = ["+","-","*","/"]
+        while token:
             element = token.pop(0)
             try:
-                element = int(element) #verificamos se o elemento é um número
+                int(element) #verificamos se o elemento é um número
                 queue.append(element)
             except:
-                operators = ["+","-","*","/"]
-                if element in operators: #verificamos se o elemento é um operador
-                    inversedSize = reversed(range(len(stack)-1)) 
-                    for i in inversedSize: #percorremos o vetor de trás p/ frente (pilha)
-                        if self.greaterPrecedence(stack[i],element):
-                            queue.append(stack[i]) #adicionamos o operator com maior precedencia da pilha à fila
-                            stack.remove(stack[i])
-                            stack.append(element) #adicionamos o operator novo à fila
-                        break    
+                if element in operators: #verificamos se o elemento é um operador e se a pilha contem elementos
+                    while stack:
+                        topOfStack = stack[-1]
+                        if self.greaterPrecedence(element,topOfStack,operators):
+                            queue.append(stack.pop()) #adicionamos o operator com maior precedencia da pilha à fila
+                        else:
+                            break    
+                    stack.append(element) #adicionamos o operator novo à pilha    
                 elif element == "(":
-                    pass
-                else element == ")"
-                    pass
+                    stack.append(element)
+                elif element == ")":
+                    while stack[-1] != "(": #verificamos se o topo da pilha contem "(" se não adicionamos os operadores à fila
+                        queue.append(stack.pop())
+                    stack.pop()        
+                else:
+                    raise ValueError(element,' é um valor inválido!')                                       
+        while stack: #Se existirem operadores ainda na pilha, mova-os à fila
+            queue.append(stack.pop()) 
+              
+        return queue
 
-class node(object):
-    def __init__(self,key):
-        self.key = key
-        self.right = None
-        self.left = None
-    
-    def lexer(self):
-        
-
-
-
+    def evalStep(self,rpn):
+        operators = ["+","-","*","/"]
+        stack = []
+        while rpn:
+            element = rpn.pop(0)
+            if element in operators:
+                operand = stack.pop()
+                operand2 = stack.pop()
+                exec('result = '+"str("+operand+element+operand2+")", locals(), globals())
+                stack.append(result)
+            else:
+                stack.append(element)    
+        return stack.pop()
 def main():
-    expression = "(2 + 5) - (3 + 7)"
-    
+    expression = input()
+    while expression:
+        postFixExpression = []
+        tree = Tree()
+        token = tree.lexer(expression)
+        rpn = tree.paser(token)
+        print(tree.evalStep(rpn))
+        # tree.postFix(rpnTree,postFixExpression)
+        # print(postFixExpression)
 
+        expression = input()
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
